d923b6e9449e10e36f39f0a3f3d3e13faa6fe8e4
robbie <desimick@gmail.com>
2023-05-10 23:12:15 +0500

    Bottles

--- p_utils/data_loader.py
+++ p_utils/data_loader.py
@@ -54,13 +54,13 @@ class CustomDataloader(Dataset):
         logging.info(f'Creating dataset with {len(self.ids)} examples')
         logging.info('Scanning mask files to determine unique values')
 
-        #with Pool() as p:
-        #    unique = list(tqdm(
-        #        p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), self.ids),
-        #        total=len(self.ids)
-        #    ))
+        with Pool() as p:
+            unique = list(tqdm(
+                p.imap(partial(unique_mask_values, mask_dir=self.mask_dir, mask_suffix=self.mask_suffix), self.ids),
+                total=len(self.ids)
+            ))
 
-        self.mask_values = [0, 1, 2] # list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
+        self.mask_values = list(sorted(np.unique(np.concatenate(unique), axis=0).tolist()))
         logging.info(f'Unique mask values: {self.mask_values}')
 
     def __len__(self):
